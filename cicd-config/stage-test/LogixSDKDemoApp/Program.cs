a9dc4cace2ceb02988a2204ebeba2f6371154550
Andre Yost <andre.yost@rockwellautomation.com>
2024-03-18 16:50:23 -0500

    updateding branch

---
+++ cicd-config/stage-test/LogixSDKDemoApp/Program.cs
@@ -0,0 +1,20 @@
+﻿using RockwellAutomation.LogixDesigner;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace LogixSDKDemoApp
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            //Path to your ACD file here.
+            string filePath = @"C:\Users\ASYost\Desktop\s5k_cicd_testfiles\CICD_test.ACD";
+            // Open the project and store the reference as myProject.
+            LogixProject myProject = await LogixProject.OpenLogixProjectAsync(filePath);
+            // Do stuff.
+            Process.Start($"GetTagValue {filePath} test_DINT");
+        }
+    }
+}
+
