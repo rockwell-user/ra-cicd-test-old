d940b627971f8581f622b9a85a57069a5d2f5f7a
Andre Yost <andre.yost@rockwellautomation.com>
2024-04-18 21:37:34 -0400

    jenkinsfile updated

--- cicd-config/stage-test/stage-test-configuration/LogixSDKDemoApp/obj/Debug/net6.0/TestStage_CICDExample.AssemblyInfo.cs
+++ cicd-config/stage-test/stage-test-configuration/LogixSDKDemoApp/obj/Debug/net6.0/TestStage_CICDExample.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCopyrightAttribute("Copyright © 2024 Rockwell Automation Technologies, Inc. All Rights Reserved.")]
 [assembly: System.Reflection.AssemblyDescriptionAttribute("C# client for the FactoryTalk Logix SDK")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.1.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.01+a1e5ca436697b36ecdd3d9f2886cd92d73ca7841")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.01+109212573030885d36defa67bd7eed859f74b6c0")]
 [assembly: System.Reflection.AssemblyProductAttribute("FactoryTalk Logix SDK Demo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestStage_CICDExample")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.1.0.0")]
