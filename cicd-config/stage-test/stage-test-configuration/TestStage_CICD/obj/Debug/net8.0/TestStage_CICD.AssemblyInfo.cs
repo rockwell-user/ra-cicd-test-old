ef73b5db3d62e859348e7e6ab57ac34ca8b6a362
Andre Yost <andre.yost@rockwellautomation.com>
2024-04-26 11:24:35 -0400

    jenkinsfile updated

--- cicd-config/stage-test/stage-test-configuration/TestStage_CICD/obj/Debug/net8.0/TestStage_CICD.AssemblyInfo.cs
+++ cicd-config/stage-test/stage-test-configuration/TestStage_CICD/obj/Debug/net8.0/TestStage_CICD.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestStage_CICD")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ac00c930de9c4365e6967877dcbec15072cfdb38")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a83fbf15d5c7d1ef98acda598e659be15b490369")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestStage_CICD")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestStage_CICD")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
