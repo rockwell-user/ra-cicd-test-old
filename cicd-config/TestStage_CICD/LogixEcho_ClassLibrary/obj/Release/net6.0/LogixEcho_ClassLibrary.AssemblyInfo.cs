ebba3155ef4f4ec35269c6ff0dbc75e325d8c22f
Andre Sandor Yost <andresyost2000@gmail.com>
2024-05-01 09:26:51 -0400

    jenkins-RA-CICD-Pipeline-217-3d53692983d2df6a5a205d50624c131ce920189c | Commiting Results for Successful Jenkins Automated Test

--- cicd-config/TestStage_CICD/LogixEcho_ClassLibrary/obj/Release/net6.0/LogixEcho_ClassLibrary.AssemblyInfo.cs
+++ cicd-config/TestStage_CICD/LogixEcho_ClassLibrary/obj/Release/net6.0/LogixEcho_ClassLibrary.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LogixEcho_ClassLibrary")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5dece6bad36847c56cf4440dfa876729cbee79c3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3d53692983d2df6a5a205d50624c131ce920189c")]
 [assembly: System.Reflection.AssemblyProductAttribute("LogixEcho_ClassLibrary")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LogixEcho_ClassLibrary")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- cicd-config/TestStage_CICD/TestScript_ConsoleApplication/obj/Release/net8.0/TestScript_ConsoleApplication.AssemblyInfo.cs
+++ cicd-config/TestStage_CICD/TestScript_ConsoleApplication/obj/Release/net8.0/TestScript_ConsoleApplication.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestScript_ConsoleApplication")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5dece6bad36847c56cf4440dfa876729cbee79c3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3d53692983d2df6a5a205d50624c131ce920189c")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestScript_ConsoleApplication")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestScript_ConsoleApplication")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
