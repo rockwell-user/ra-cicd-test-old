696163ec351dd9537f0b59ee8d338c75d8ffb064
Andre Yost <andre.yost@rockwellautomation.com>
2024-05-06 10:27:57 -0400

    excel file modification

--- cicd-config/TestStage_CICD/TestScript_ConsoleApplication/obj/Release/net8.0/TestScript_ConsoleApplication.AssemblyInfo.cs
+++ cicd-config/TestStage_CICD/TestScript_ConsoleApplication/obj/Release/net8.0/TestScript_ConsoleApplication.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestScript_ConsoleApplication")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d65fd816af749932f53e0bbb52134e3f7518f0b4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+877d18bfec8e7cdde44f8bfacaec5deba548f133")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestScript_ConsoleApplication")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestScript_ConsoleApplication")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
