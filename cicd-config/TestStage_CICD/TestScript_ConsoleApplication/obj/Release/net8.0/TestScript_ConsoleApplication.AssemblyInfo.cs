23c5cf36f4456b53b8ba365b63f0cdc4e8b3c034
Andre <andre.yost@rockwellautomation.com>
2024-05-10 10:50:17 -0400

    TEST SUCCESS! | Jenkins job: RA-CICD-TEST | Jenkins build #: 274 | Commit hash on which testing was conducted: 6362fc784bf75cb551e4c3dbe577167e7a12cc8c

--- cicd-config/TestStage_CICD/TestScript_ConsoleApplication/obj/Release/net8.0/TestScript_ConsoleApplication.AssemblyInfo.cs
+++ cicd-config/TestStage_CICD/TestScript_ConsoleApplication/obj/Release/net8.0/TestScript_ConsoleApplication.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestScript_ConsoleApplication")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+574ad587e61f9f9c28a1616dff4310aeec971e98")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6362fc784bf75cb551e4c3dbe577167e7a12cc8c")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestScript_ConsoleApplication")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestScript_ConsoleApplication")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
